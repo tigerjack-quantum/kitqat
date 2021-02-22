4d088c93cb9746c3e233016caa28c884099df338
Simone Perriello <simone.perriello@pm.me>
2021-02-22 06:40:46 +0100

    tests: added different simulator options

--- test/common_circuit.py
+++ test/common_circuit.py
@@ -18,12 +18,40 @@ class CircuitTestCase(BasicTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        print("using simulator: ", end="")
         if cls.QLM_ON == '0':
+            print("PyLinalg")
             from qat.qpus import PyLinalg
             cls.qpu = PyLinalg()
         else:
-            from qat.qpus import LinAlg
-            cls.qpu = LinAlg()
+            if cls.QLM_ON.lower() == 'stabs':
+                print("Stabs")
+                from qat.qpus import Stabs
+                from qat.external.utils.mcx import ccnot
+                cls.qpu = Stabs()
+                cls.links = [ccnot]
+            elif cls.QLM_ON.lower() == 'feynman':
+                print("Feynman")
+                from qat.qpus import Feynman
+                cls.qpu = Feynman()
+                cls.links = []
+            elif cls.QLM_ON.lower() == 'mps':
+                print("MPS")
+                from qat.qpus import MPS
+                cls.qpu = MPS(lnnize=True)
+                cls.links = []
+            else:
+                # default to linalg
+                from qat.qpus import LinAlg
+                cls.qpu = LinAlg()
+                cls.links = []
+
+    @classmethod
+    def simulate_program(cls, program, circ_args={}, job_args={}):
+        cr = program.to_circ(*circ_args)
+        jb = cr.to_job(*job_args)
+        res = cls.qpu.submit(jb)
+        return res
 
     @staticmethod
     def draw_circuit(program: Union['Program', 'Circuit'], **kwargs):
