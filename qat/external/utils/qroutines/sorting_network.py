399e82254e3c44c0b15b33d5123c760969bff446
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2021-03-09 22:36:49 +0100

    cleaning

--- qat/external/utils/qroutines/sorting_network.py
+++ qat/external/utils/qroutines/sorting_network.py
@@ -27,9 +27,9 @@ def build_gate_swap_cols(nrows):
 
 @build_gate("SORTING_NETWORK_COLS", [int, dict])
 def build_gate_sorting_network_cols(
-        nrows: int, benes_pattern_dict: Dict[str, Any]) -> QRoutine:
-    ncols: int = benes_pattern_dict['n_lines']
-    comp_len: int = benes_pattern_dict['n_comps']
+        nrows: int, net_data: Dict[str, Any]) -> QRoutine:
+    ncols: int = net_data['n_lines']
+    comp_len: int = net_data['n_comps']
 
     routine = QRoutine()
     row_wires = []
@@ -41,19 +41,19 @@ def build_gate_sorting_network_cols(
     comp = routine.new_wires(comp_len)
 
     qrout = build_gate_swap_cols(nrows)
-    for pattern in benes_pattern_dict['swaps_pattern']:
+    for pattern in net_data['swaps_pattern']:
         routine.apply(qrout.ctrl(), comp[pattern[0]], col_wires[pattern[1]],
                       col_wires[pattern[2]])
     return routine
 
 
-def _build_gate_common(pattern: Dict[str, Any]) -> QRoutine:
-    a_len: int = pattern['n_lines']
-    comp_len: int = pattern['n_comps']
+def _build_gate_common(net_data: Dict[str, Any]) -> QRoutine:
+    a_len: int = net_data['n_lines']
+    comp_len: int = net_data['n_comps']
     routine = QRoutine()
     a_wires = routine.new_wires(a_len)
     comp_wires = routine.new_wires(comp_len)
-    for swap_pattern in pattern['swaps_pattern']:
+    for swap_pattern in net_data['swaps_pattern']:
         # Compare qubits 1 and 2 and put the output in pattern 0
         routine.apply(two_bit_comparator(), a_wires[swap_pattern[1]],
                       a_wires[swap_pattern[2]], comp_wires[swap_pattern[0]])
@@ -63,8 +63,8 @@ def _build_gate_common(pattern: Dict[str, Any]) -> QRoutine:
 
 
 @build_gate("BITONIC_SORTER", [dict])
-def build_gate_bitonic_sorter(pattern: Dict[str, Any]) -> QRoutine:
-    return _build_gate_common(pattern)
+def build_gate_bitonic_sorter(net_data: Dict[str, Any]) -> QRoutine:
+    return _build_gate_common(net_data)
 
 
 def get_pattern_bitonic_sorter(n) -> Dict[str, Any]:
@@ -85,19 +85,19 @@ def get_pattern_bitonic_sorter(n) -> Dict[str, Any]:
     - the first line involved in the swap
     - the second line involved in the swap
     """
-    nwr_dict = {}
+    net_data = {}
     steps = int(np.ceil(np.log2(n)))
-    nwr_dict['n_lines'] = 2**steps
-    nwr_dict['swaps_pattern'] = []
-    initial_swaps = int(nwr_dict['n_lines'] / 2)
+    net_data['n_lines'] = 2**steps
+    net_data['swaps_pattern'] = []
+    initial_swaps = int(net_data['n_lines'] / 2)
 
-    _get_pattern_bitonic_sorter(0, initial_swaps, int(nwr_dict['n_lines'] / 2),
-                                0, nwr_dict)
-    nwr_dict['n_comps'] = len(nwr_dict['swaps_pattern'])
-    return nwr_dict
+    _get_pattern_bitonic_sorter(0, initial_swaps, int(net_data['n_lines'] / 2),
+                                0, net_data)
+    net_data['n_comps'] = len(net_data['swaps_pattern'])
+    return net_data
 
 
-def _get_pattern_bitonic_sorter(start, end, swap_step, comp_q_idx, nwr_dict):
+def _get_pattern_bitonic_sorter(start, end, swap_step, comp_q_idx, net_data):
     _LOGGER.debug("Start: %d, end: %d, swap_step: %d", start, end, swap_step)
     if (swap_step == 0 or start >= end):
         _LOGGER.debug("Base case recursion")
@@ -107,7 +107,7 @@ def _get_pattern_bitonic_sorter(start, end, swap_step, comp_q_idx, nwr_dict):
     for i in range(start, end):
         for_iter += 1
         _LOGGER.info("cswap(%d, %d, %d)", comp_q_idx, i, i + swap_step)
-        nwr_dict['swaps_pattern'].append((comp_q_idx, i, i + swap_step))
+        net_data['swaps_pattern'].append((comp_q_idx, i, i + swap_step))
         comp_q_idx += 1
 
     for_iter_next = min(for_iter, int(swap_step / 2))
@@ -116,78 +116,77 @@ def _get_pattern_bitonic_sorter(start, end, swap_step, comp_q_idx, nwr_dict):
         start, end, swap_step, for_iter_next)
     comp_q_idx = _get_pattern_bitonic_sorter(start, start + for_iter_next,
                                              int(swap_step / 2), comp_q_idx,
-                                             nwr_dict)
+                                             net_data)
     _LOGGER.debug(
         "Before rec, start: %d, end: %d, swap_step: %d, for_iter_next %d",
         start, end, swap_step, for_iter_next)
     comp_q_idx = _get_pattern_bitonic_sorter(start + swap_step,
                                              start + swap_step + for_iter_next,
                                              int(swap_step / 2), comp_q_idx,
-                                             nwr_dict)
+                                             net_data)
     return comp_q_idx
 
 
 @build_gate("MERGER", [dict])
-def build_gate_merger(pattern: dict):
-    return _build_gate_common(pattern)
+def build_gate_merger(net_data: dict):
+    return _build_gate_common(net_data)
 
 
 def get_pattern_merger(n):
-    nwr_dict = {}
-    comp_q_idx = _get_pattern_merger_support(n, nwr_dict, 0)
+    net_data = {}
+    comp_q_idx = _get_pattern_merger_support(n, net_data, 0)
 
-    nwr_dict['n_comps'] = len(nwr_dict['swaps_pattern'])
-    return nwr_dict
+    net_data['n_comps'] = len(net_data['swaps_pattern'])
+    return net_data
 
 
-def _get_pattern_merger_support(n, nwr_dict, comp_q_idx, start_shift=0):
+def _get_pattern_merger_support(n, net_data, comp_q_idx, start_shift=0):
     steps = int(np.ceil(np.log2(n)))
-    nwr_dict['n_lines'] = 2**steps
-    nwr_dict['swaps_pattern'] = nwr_dict.get('swaps_pattern', [])
-    initial_swaps = int(nwr_dict['n_lines'] / 2)
+    net_data['n_lines'] = 2**steps
+    net_data['swaps_pattern'] = net_data.get('swaps_pattern', [])
+    initial_swaps = int(net_data['n_lines'] / 2)
 
     for i in range(initial_swaps):
-        nwr_dict['swaps_pattern'].append(
+        net_data['swaps_pattern'].append(
             (comp_q_idx, i + start_shift,
-             nwr_dict['n_lines'] - i - 1 + start_shift))
+             net_data['n_lines'] - i - 1 + start_shift))
         comp_q_idx += 1
 
     # the second half of the circuit is identical to the bitonic sorter
     start = start_shift
     swap_step = int(initial_swaps / 2)
     comp_q_idx = _get_pattern_bitonic_sorter(start, start + swap_step,
-                                             swap_step, comp_q_idx, nwr_dict)
+                                             swap_step, comp_q_idx, net_data)
     comp_q_idx = _get_pattern_bitonic_sorter(start + swap_step * 2,
                                              start + swap_step * 3, swap_step,
-                                             comp_q_idx, nwr_dict)
+                                             comp_q_idx, net_data)
     return comp_q_idx
 
 @build_gate("SORTER", [dict])
-def build_gate_sorter(pattern):
-    return _build_gate_common(pattern)
+def build_gate_sorter(net_data):
+    return _build_gate_common(net_data)
 
 
 def get_pattern_sorter(n):
-    nwr_dict = {}
+    net_data = {}
     lis = []
     print("")
 
     _get_pattern_sorter_support(0, n, lis)
 
     comp_q_idx = 0
+    # Note that, since the last pattern to be analyzed is the greatest one, we
+    # obtain as side effect the right number of 'n_lines'
     for (start, end) in reversed(lis):
         n = len(range(start, end))
-        comp_q_idx = _get_pattern_merger_support(n, nwr_dict, comp_q_idx,
+        comp_q_idx = _get_pattern_merger_support(n, net_data, comp_q_idx,
                                                  start)
-    # for k, v in nwr_dict.items():
-    #     print(k)
-    #     print(v)
-        nwr_dict['n_comps'] = len(nwr_dict['swaps_pattern'])
-    return nwr_dict
+        net_data['n_comps'] = len(net_data['swaps_pattern'])
+    return net_data
 
 
 def _get_pattern_sorter_support(start, end, acc, depth=0):
-    rec_string = '>' * depth
+    # rec_string = '>' * depth
     # print(f"{rec_string}recursion start")
     # print(f"{rec_string}merger [{start}-{end})")
     pattern = (start, end)
