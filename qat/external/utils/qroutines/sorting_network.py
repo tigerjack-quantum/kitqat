d7606e8db639d7f860013d29f39263c29328419f
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2021-03-09 15:58:30 +0100

    Added routines and tests for sorting networks

--- qat/external/utils/qroutines/sorting_network.py
+++ qat/external/utils/qroutines/sorting_network.py
@@ -44,26 +44,30 @@ def build_gate_sorting_network_cols(
     for pattern in benes_pattern_dict['swaps_pattern']:
         routine.apply(qrout.ctrl(), comp[pattern[0]], col_wires[pattern[1]],
                       col_wires[pattern[2]])
-
     return routine
 
 
-@build_gate("BITONIC_SORTING", [dict])
-def build_gate_bitnonic_sorting(
-        benes_pattern_dict: Dict[str, Any]) -> QRoutine:
-    a_len: int = benes_pattern_dict['n_lines']
-    comp_len: int = benes_pattern_dict['n_comps']
-    routine = QRoutine(arity=a_len + comp_len)
-    # We use numbers directly, not wires
-    for pattern in benes_pattern_dict['swaps_pattern']:
+def _build_gate_common(pattern: Dict[str, Any]) -> QRoutine:
+    a_len: int = pattern['n_lines']
+    comp_len: int = pattern['n_comps']
+    routine = QRoutine()
+    a_wires = routine.new_wires(a_len)
+    comp_wires = routine.new_wires(comp_len)
+    for swap_pattern in pattern['swaps_pattern']:
         # Compare qubits 1 and 2 and put the output in pattern 0
-        routine.apply(two_bit_comparator(), pattern[1], pattern[2],
-                      a_len + pattern[0])
-        routine.apply(SWAP.ctrl(), a_len + pattern[0], pattern[1], pattern[2])
+        routine.apply(two_bit_comparator(), a_wires[swap_pattern[1]],
+                      a_wires[swap_pattern[2]], comp_wires[swap_pattern[0]])
+        routine.apply(SWAP.ctrl(), comp_wires[swap_pattern[0]],
+                      a_wires[swap_pattern[1]], a_wires[swap_pattern[2]])
     return routine
 
 
-def get_pattern_bitonic_sorting(n) -> Dict[str, Any]:
+@build_gate("BITONIC_SORTER", [dict])
+def build_gate_bitonic_sorter(pattern: Dict[str, Any]) -> QRoutine:
+    return _build_gate_common(pattern)
+
+
+def get_pattern_bitonic_sorter(n) -> Dict[str, Any]:
     """Given how it's built, n should be a power of 2 and, if not, it returns the
     combination rounding up to the top power of 2. If the original n is not a
     power of 2, you may want to adapt the circuit avoiding the use of the last
@@ -87,24 +91,13 @@ def get_pattern_bitonic_sorting(n) -> Dict[str, Any]:
     nwr_dict['swaps_pattern'] = []
     initial_swaps = int(nwr_dict['n_lines'] / 2)
 
-    _get_pattern_bitonic_network(0, initial_swaps,
-                                 int(nwr_dict['n_lines'] / 2), 0, nwr_dict)
+    _get_pattern_bitonic_sorter(0, initial_swaps, int(nwr_dict['n_lines'] / 2),
+                                0, nwr_dict)
     nwr_dict['n_comps'] = len(nwr_dict['swaps_pattern'])
     return nwr_dict
 
 
-def _get_generate_pattern_support_step2(start, end, swap_step, comp_q_idx,
-                                        nwr_dict):
-
-    pass
-
-
-def _get_generate_pattern_support_step3(start, end, swap_step, comp_q_idx,
-                                        nwr_dict):
-    pass
-
-
-def _get_pattern_bitonic_network(start, end, swap_step, comp_q_idx, nwr_dict):
+def _get_pattern_bitonic_sorter(start, end, swap_step, comp_q_idx, nwr_dict):
     _LOGGER.debug("Start: %d, end: %d, swap_step: %d", start, end, swap_step)
     if (swap_step == 0 or start >= end):
         _LOGGER.debug("Base case recursion")
@@ -121,13 +114,91 @@ def _get_pattern_bitonic_network(start, end, swap_step, comp_q_idx, nwr_dict):
     _LOGGER.debug(
         "Before rec1, start: %d, end: %d, swap_step: %d, for_iter_next %d",
         start, end, swap_step, for_iter_next)
-    comp_q_idx = _get_pattern_bitonic_network(start, start + for_iter_next,
-                                              int(swap_step / 2), comp_q_idx,
-                                              nwr_dict)
+    comp_q_idx = _get_pattern_bitonic_sorter(start, start + for_iter_next,
+                                             int(swap_step / 2), comp_q_idx,
+                                             nwr_dict)
     _LOGGER.debug(
         "Before rec, start: %d, end: %d, swap_step: %d, for_iter_next %d",
         start, end, swap_step, for_iter_next)
-    comp_q_idx = _get_pattern_bitonic_network(
-        start + swap_step, start + swap_step + for_iter_next,
-        int(swap_step / 2), comp_q_idx, nwr_dict)
+    comp_q_idx = _get_pattern_bitonic_sorter(start + swap_step,
+                                             start + swap_step + for_iter_next,
+                                             int(swap_step / 2), comp_q_idx,
+                                             nwr_dict)
     return comp_q_idx
+
+
+@build_gate("MERGER", [dict])
+def build_gate_merger(pattern: dict):
+    return _build_gate_common(pattern)
+
+
+def get_pattern_merger(n):
+    nwr_dict = {}
+    comp_q_idx = _get_pattern_merger_support(n, nwr_dict, 0)
+
+    nwr_dict['n_comps'] = len(nwr_dict['swaps_pattern'])
+    return nwr_dict
+
+
+def _get_pattern_merger_support(n, nwr_dict, comp_q_idx, start_shift=0):
+    steps = int(np.ceil(np.log2(n)))
+    nwr_dict['n_lines'] = 2**steps
+    nwr_dict['swaps_pattern'] = nwr_dict.get('swaps_pattern', [])
+    initial_swaps = int(nwr_dict['n_lines'] / 2)
+
+    for i in range(initial_swaps):
+        nwr_dict['swaps_pattern'].append(
+            (comp_q_idx, i + start_shift,
+             nwr_dict['n_lines'] - i - 1 + start_shift))
+        comp_q_idx += 1
+
+    # the second half of the circuit is identical to the bitonic sorter
+    start = start_shift
+    swap_step = int(initial_swaps / 2)
+    comp_q_idx = _get_pattern_bitonic_sorter(start, start + swap_step,
+                                             swap_step, comp_q_idx, nwr_dict)
+    comp_q_idx = _get_pattern_bitonic_sorter(start + swap_step * 2,
+                                             start + swap_step * 3, swap_step,
+                                             comp_q_idx, nwr_dict)
+    return comp_q_idx
+
+@build_gate("SORTER", [dict])
+def build_gate_sorter(pattern):
+    return _build_gate_common(pattern)
+
+
+def get_pattern_sorter(n):
+    nwr_dict = {}
+    lis = []
+    print("")
+
+    _get_pattern_sorter_support(0, n, lis)
+
+    comp_q_idx = 0
+    for (start, end) in reversed(lis):
+        n = len(range(start, end))
+        comp_q_idx = _get_pattern_merger_support(n, nwr_dict, comp_q_idx,
+                                                 start)
+    # for k, v in nwr_dict.items():
+    #     print(k)
+    #     print(v)
+        nwr_dict['n_comps'] = len(nwr_dict['swaps_pattern'])
+    return nwr_dict
+
+
+def _get_pattern_sorter_support(start, end, acc, depth=0):
+    rec_string = '>' * depth
+    # print(f"{rec_string}recursion start")
+    # print(f"{rec_string}merger [{start}-{end})")
+    pattern = (start, end)
+    acc.append(pattern)
+    if (start + 2 >= end):
+        # input(f"{rec_string}base case")
+        return
+
+    mid = int((start + end) / 2)
+    # print(f"{rec_string}before recursion 1")
+    _get_pattern_sorter_support(start, mid, acc, depth + 1)
+    # print(f"{rec_string}before recursion 2")
+    _get_pattern_sorter_support(mid, end, acc, depth + 1)
+    return
---
+++ test/test_qroutine_sorting_network.py
@@ -0,0 +1,136 @@
+from test.common_circuit import CircuitTestCase
+
+from parameterized import parameterized
+from qat.external.utils.qroutines import sorting_network as sn
+from qat.external.utils.qroutines import qregs_init as qregs
+from qat.lang.AQASM.program import Program
+
+# import unittest
+
+
+class SortingNetworkTestCase(CircuitTestCase):
+    LITTLE_ENDIAN = False
+
+    @classmethod
+    def _check_bitonic(cls, string):
+        monotonicity_changes = 0
+        for i in range(1, len(string)):
+            if string[i] < string[i - 1] or string[i] > string[i - 1]:
+                monotonicity_changes += 1
+            if monotonicity_changes > 2:
+                return False
+        return True
+
+    @classmethod
+    def _check_halves_sorted(cls, string):
+        halflen = int(len(string) / 2)
+        st1, st2 = string[:halflen], string[halflen:halflen * 2]
+
+        is_sorted_1 = all(st1[i] <= st1[i + 1] for i in range(halflen - 1))
+
+        return is_sorted_1 and all(st2[i] <= st2[i + 1]
+                                   for i in range(halflen - 1))
+
+    def _prepare_circuit(self, string, pattern):
+        self.pr = Program()
+        self.qr = self.pr.qalloc(pattern['n_lines'])
+        self.comps = self.pr.qalloc(pattern['n_comps'])
+        init = qregs.initialize_qureg_given_bitstring(string,
+                                                      self.LITTLE_ENDIAN)
+        self.pr.apply(init, self.qr)
+
+    @parameterized.expand([
+        "0000",
+        "1111",
+        "0110",
+        "1010",
+        "10010000",
+        "11000011",
+    ])
+    # @unittest.skip("WORKING")
+    def test_bitonic_sorter(self, string):
+        is_bitonic = self._check_bitonic(string)
+        n = len(string)
+        # print(is_bitonic)
+        pattern = sn.get_pattern_bitonic_sorter(n)
+        self._prepare_circuit(string, pattern)
+
+        qrout = sn.build_gate_bitonic_sorter(pattern)
+        self.pr.apply(qrout, self.qr, self.comps)
+        cr = self.pr.to_circ()
+
+        res = self.simulate_program(self.pr, job_args={'qubits': self.qr})
+        obtained = res.raw_data[0].state.bitstring
+
+        is_sorted = all(obtained[i] <= obtained[i + 1]
+                        for i in range(len(string) - 1))
+        sorted_string_exp = ''.join(list(sorted(string)))
+
+        if is_bitonic:
+            self.assertTrue(is_sorted)
+            self.assertEqual(obtained, sorted_string_exp)
+
+    @parameterized.expand([
+        "01",
+        "10",
+        "0000",
+        "1111",
+        "0101",
+        "1101",
+        "00110111",
+    ])
+    # @unittest.skip("WORKING")
+    def test_merger(self, string):
+        are_sorted = self._check_halves_sorted(string)
+        n = len(string)
+        pattern = sn.get_pattern_merger(n)
+        self._prepare_circuit(string, pattern)
+
+        qrout = sn.build_gate_merger(pattern)
+        self.pr.apply(qrout, self.qr, self.comps)
+        cr = self.pr.to_circ()
+        # self.draw_circuit(cr, max_depth=2)
+
+        res = self.simulate_program(self.pr, job_args={'qubits': self.qr})
+        obtained = res.raw_data[0].state.bitstring
+
+        is_sorted = all(obtained[i] <= obtained[i + 1]
+                        for i in range(len(string) - 1))
+        sorted_string_exp = ''.join(list(sorted(string)))
+
+        if are_sorted:
+            self.assertTrue(is_sorted)
+            self.assertEqual(obtained, sorted_string_exp)
+
+    @parameterized.expand([
+        "01",
+        "10",
+        "0000",
+        "1111",
+        "0001",
+        "0010",
+        "0111",
+        "1011",
+        "1001",
+        # "10110111",
+    ])
+    def test_sorter(self, string):
+        n = len(string)
+        pattern = sn.get_pattern_sorter(n)
+
+        self._prepare_circuit(string, pattern)
+
+        qrout = sn.build_gate_merger(pattern)
+        self.pr.apply(qrout, self.qr, self.comps)
+        # cr = self.pr.to_circ()
+        print(self.pr.qbit_count)
+
+        res = self.simulate_program(self.pr, job_args={'qubits': self.qr})
+        obtained = res.raw_data[0].state.bitstring
+
+        is_sorted = all(obtained[i] <= obtained[i + 1]
+                        for i in range(len(string) - 1))
+        sorted_string_exp = ''.join(list(sorted(string)))
+
+        self.assertTrue(is_sorted)
+        self.assertEqual(obtained, sorted_string_exp)
