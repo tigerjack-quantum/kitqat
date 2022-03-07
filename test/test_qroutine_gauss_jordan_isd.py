fc849883a9d68c43dd5a8396f0203d791d38ae71
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-03-07 17:54:11 +0100

    gje: add partial test

---
+++ test/test_qroutine_gauss_jordan_isd.py
@@ -0,0 +1,142 @@
+import unittest
+from test.common_circuit import CircuitTestCase
+
+import numpy as np
+from parameterized import parameterized
+from qat.external.utils.qroutines.linalg import gauss_jordan_isd as gji
+from qat.external.utils.qroutines.linalg import rref
+from qat.external.utils.qroutines.linalg import matrix as qmatrix
+from qat.lang.AQASM.program import Program
+from sympy import Matrix
+
+# from qat.core.util import statistics
+
+
+class GjiTestCase(CircuitTestCase):
+
+    def _prepare_circuit(self, matrix):
+        self.pr = Program()
+        nrows, ncols = matrix.shape
+        self.nsquare = min(matrix.shape)
+        # qrout = qmatrix.initialize_qureg_to_binary_matrix(matrix.tolist())
+        qrout = qmatrix.initialize_qureg_to_binary_matrix(matrix)
+        qr_matrix = self.pr.qalloc(nrows * ncols)
+        self.pr.apply(qrout, qr_matrix)
+        self.qregs_rows = qmatrix.get_rows_as_qubit_list(
+            nrows, ncols, qr_matrix)
+
+        self.qbit_range = set(q.index for qreg in self.qregs_rows
+                              for q in qreg)
+        swap_anc_n, add_anc_n = gji.get_required_ancillae(nrows)
+        self.add_qregs = self.pr.qalloc(add_anc_n)
+        self.swap_qregs = self.pr.qalloc(swap_anc_n)
+
+    def _common_test(self,
+                     matrix,
+                     test_u=False,
+                     test_iden=False,
+                     should_fail=False):
+        self._prepare_circuit(matrix)
+
+        nrows, ncols = matrix.shape
+        gji_gate = gji.get_rref(nrows, ncols, False)
+        self.pr.apply(gji_gate, self.qregs_rows, self.swap_qregs,
+                      self.add_qregs)
+
+        if test_u:
+            self.pr.measure(qbits=self.swap_qregs)
+            self.pr.measure(qbits=self.add_qregs)
+        cr = self.pr.to_circ()
+        # print(statistics(cr))
+
+        res = self.qpu.submit(cr.to_job(qubits=self.qbit_range))
+
+        sample = res[0]
+        mat_gji = qmatrix.build_matrix_from_sample(sample, self.qbit_range,
+                                                    matrix.shape)
+        mat_gji_sim = Matrix(matrix).rref(pivots=False)
+        # The gjis are expected to be different
+        if should_fail:
+            with self.assertRaises(AssertionError):
+                np.testing.assert_array_equal(mat_gji, mat_gji_sim)
+        else:
+            np.testing.assert_array_equal(mat_gji, mat_gji_sim)
+
+        # The gjis are not necessarily different, but for sure the obtained
+        # gji has not an IDENTITY matrix in the left part
+        if test_iden:
+            if should_fail:
+                with self.assertRaises(AssertionError):
+                    np.testing.assert_array_equal(
+                        mat_gji[:self.nsquare, :self.nsquare],
+                        np.eye(self.nsquare))
+            else:
+                np.testing.assert_array_equal(mat_gji, mat_gji_sim)
+
+        # The matrix of transformations U can be reconstructed from the
+        # ancillae.
+        if test_u:
+            u = rref.build_u_matrix_from_sample(sample, self.nsquare)
+            np.testing.assert_array_equal(u @ matrix % 2, mat_gji)
+
+    @parameterized.expand([
+        ("3x3", np.array([[0, 1, 1], [1, 0, 1], [0, 0, 1]])),
+        ("3x4", np.array([[1, 1, 0, 0], [1, 0, 0, 0], [0, 1, 1, 1]])),
+        ("3x4", np.array([[0, 1, 1, 1], [1, 0, 0, 1], [0, 0, 1, 1]])),
+    ])
+    def test_equals_iden(self, name, matrix):
+        """They should give the same results of a normal GJI and an identity matrix on
+        the left
+
+        """
+        self.logger.debug("test with %s", name)
+        self._common_test(matrix, True, True, False)
+
+    @parameterized.expand([
+        ("3x5", np.array([[0, 1, 1, 1, 0], [0, 1, 0, 0, 0], [1, 1, 0, 0, 1]])),
+    ])
+    @unittest.skipUnless(CircuitTestCase.SLOW_TEST_ON,
+                         CircuitTestCase.SLOW_TEST_ON_REASON)
+    def test_equals_iden_slow(self, name, matrix):
+        self.logger.debug("test with %s", name)
+        self._common_test(matrix, True, True, False)
+
+    @parameterized.expand([
+        ("3x3", np.array([[0, 1, 1], [0, 0, 1], [0, 1, 1]])),
+        ("3x4", np.array([[0, 0, 0, 1], [1, 0, 0, 1], [0, 0, 0, 1]])),
+    ])
+    def test_not_equals_not_iden(self, name, matrix):
+        """They should give different w.r.t. a normal GJI, and also no identity
+        """
+        self.logger.debug("test with %s", name)
+        self._common_test(matrix, True, True, True)
+
+    @parameterized.expand([("3x5",
+                            np.array([[0, 0, 0, 1, 1], [0, 1, 0, 0, 0],
+                                      [0, 1, 1, 0, 1]]))])
+    @unittest.skipUnless(CircuitTestCase.SLOW_TEST_ON,
+                         CircuitTestCase.SLOW_TEST_ON_REASON)
+    def test_not_equals_not_iden_slow(self, name, matrix):
+        self.logger.debug("test with %s", name)
+        self._common_test(matrix, True, True, True)
+
+    @parameterized.expand([
+        ("3x4", np.array([[1, 1, 1, 0], [1, 1, 1, 0], [1, 0, 0, 0]])),
+        ("3x4", np.array([[0, 0, 0, 1], [1, 1, 1, 0], [1, 0, 0, 1]])),
+    ])
+    def test_equals_not_iden(self, name, matrix):
+        """They do not necessarily give the same results of the reversible circuit
+        w.r.t. the normal RREF. For sure they do not produce identities.
+
+        """
+        self.logger.debug("test with %s", name)
+        self._common_test(matrix, True, True, True)
+
+    @parameterized.expand([
+        ("3x5", np.array([[1, 0, 0, 1, 0], [0, 0, 0, 0, 0], [1, 1, 0, 1, 1]])),
+    ])
+    @unittest.skipUnless(CircuitTestCase.SLOW_TEST_ON,
+                         CircuitTestCase.SLOW_TEST_ON_REASON)
+    def test_equals_not_iden_slow(self, name, matrix):
+        self.logger.debug("test with %s", name)
+        self._common_test(matrix, True, False, False)
