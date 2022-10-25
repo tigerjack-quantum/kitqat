e6ed06fa60c085e255b87274a465fcdcdd2304ee
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-10-25 18:15:30 +0200

    Add conversion program -> rprogram

--- qat/external/qpus/reversible.py
+++ qat/external/qpus/reversible.py
@@ -1,51 +1,63 @@
 """For now it is only a test bench, creating a fake Program object.
  Virtually, it should be integrated into qat, taking a circuit as input and running the simulation.
 """
+from __future__ import annotations
+
 import logging
 import operator
 from enum import Enum, auto
-from typing import Optional, Set, Union
+from typing import TYPE_CHECKING, Optional, Sequence, Set, Union
+
+from qat.lang.AQASM.gates import PredefGate
+from qat.lang.AQASM.gates import Gate
+if TYPE_CHECKING:
+    from qat.lang.AQASM.program import Program
 
 from bitarray import bitarray
 
 logger = logging.getLogger(__name__)
 
 
-class ReversibleGate(Enum):
+class RGate(Enum):
     NOT = auto()
     SWAP = auto()
 
 
-class ReversibleQPU():
+class RProgram():
+
+    rev_gate_names = ('X', 'NOT', 'SWAP')
 
     def __init__(self):
-        "docstring"
         self._ops = [
         ]  # should contain the list of operations for logging purposes
-        self.qbits: Optional[bitarray] = None
+        self.qbits: bitarray = bitarray()
 
     def qalloc(self, n=1):
-        self.qbits = bitarray('0' * n)
+        self.qbits.extend('0' * n)
 
-    def apply(self, gate: ReversibleGate, ctrls: Optional[Set[int]],
+    def apply(self, gate: RGate, ctrls: Optional[Set[int]],
               trgts: Union[int, Set[int]]):
         if self.qbits is None:
             raise AttributeError("You should initialize your qubits")
-        # if len(trgts) == 0:
-        #     return
 
-        ctrl = ctrls is None or len(ctrls) == 0 or all(
-            operator.itemgetter(*ctrls)(self.qbits))
+        # print(ctrls)
+        # if ctrls is not None:
+        #     print(len(ctrls))
+
+        ctrl = (ctrls is None or len(ctrls) == 0) or (
+            len(ctrls) == 1 and operator.itemgetter(*ctrls)(self.qbits)
+            == 1) or (len(ctrls) > 1
+                      and all(operator.itemgetter(*ctrls)(self.qbits)))
         if not ctrl:
             return
         if isinstance(trgts, int):
             trgts = {trgts}
         if ctrls is not None and not ctrls.isdisjoint(trgts):
             raise ValueError("The target and control set should be disjoint")
-        if gate == ReversibleGate.NOT:
+        if gate == RGate.NOT:
             for trgt in trgts:
                 self.qbits.invert(trgt)
-        elif gate == ReversibleGate.SWAP:
+        elif gate == RGate.SWAP:
             if len(trgts) == 2:
                 _trgts = list(trgts)
                 self.qbits[_trgts[1]], self.qbits[_trgts[0]] = self.qbits[
@@ -54,3 +66,52 @@ class ReversibleQPU():
                 raise ValueError("Swap gates can have only 2 targets")
         else:
             raise ValueError(f"Unknown gate {gate}")
+
+    @classmethod
+    def _get_and_apply_gate(cls, rprogram: RProgram, gate: Gate,
+                            qbits: Sequence[int]):
+        if not gate.name.endswith(cls.rev_gate_names):
+            raise AttributeError(
+                "Reversible gates are only NOT (X), SWAP and their controlled versions"
+            )
+        if gate.name == 'SWAP':
+            ctrls = set(qbits[:-2])
+            trgts = set(qbits[-2:])
+            rgate = RGate.SWAP
+        elif gate.name == 'X':
+            if len(qbits) == 2:
+                gate.name = 'CNOT'
+                return cls._get_and_apply_gate(rprogram, gate, qbits)
+            elif len(qbits) == 3:
+                gate.name = 'CCNOT'
+                return cls._get_and_apply_gate(rprogram, gate, qbits)
+            elif len(qbits) == 1:
+                rgate = RGate.NOT
+                trgts = {qbits[-1]}
+                ctrls = set()
+        elif gate.name == 'CNOT':
+            ctrls = {qbits[0]}
+            trgts = {qbits[1]}
+            rgate = RGate.NOT
+        elif gate.name == 'CCNOT':
+            ctrls = {qbits[0], qbits[1]}
+            trgts = {qbits[2]}
+            rgate = RGate.NOT
+        else:
+            raise Exception(
+                f"Got an unknown gate that passed the first check {gate.name}")
+
+        rprogram.apply(rgate, ctrls, trgts)
+
+    @classmethod
+    def program_to_rprogram(cls, qprogram: Program) -> RProgram:
+        rprogram = RProgram()
+        rprogram.qalloc(qprogram.qbit_count)
+        for op in qprogram.op_list:
+            qbits = [qb.index for qb in op.qbits]
+            if isinstance(op.gate, PredefGate):
+                cls._get_and_apply_gate(rprogram, op.gate, qbits)
+            elif op.gate.subgate is not None and isinstance(
+                    op.gate.subgate, PredefGate):
+                cls._get_and_apply_gate(rprogram, op.gate.subgate, qbits)
+        return rprogram
---
+++ test/test_rcircuit.py
@@ -0,0 +1,80 @@
+import functools
+from test.common import BasicTestCase
+
+from qat.external.qpus.reversible import RGate, RProgram
+from qat.lang.AQASM.program import Program
+from qat.lang.AQASM.gates import H, X, CNOT, CCNOT, SWAP
+from qat.qpus import PyLinalg
+
+
+class TestRCircuit(BasicTestCase):
+    nqbits = 10
+
+    def setUp(self):
+        super().setUp()
+        self.rpu = RProgram()
+        self.rpu.qalloc(self.nqbits)
+        self._test_arr = ['0'] * self.nqbits
+
+    def test_not(self):
+        self.rpu.apply(RGate.NOT, None, 1)
+        self._test_arr[1] = '1'
+        self.assertEqual(self.rpu.qbits.to01(), ''.join(self._test_arr))
+
+    def test_mnot(self):
+        lst = {1, 3, 4, 9}
+        self.rpu.apply(RGate.NOT, None, lst)
+        for i in lst:
+            self._test_arr[i] = '1'
+        self.assertEqual(self.rpu.qbits.to01(), ''.join(self._test_arr))
+
+    def test_swap(self):
+        self.rpu.apply(RGate.NOT, None, 3)
+        self.rpu.apply(RGate.SWAP, None, {3, 2})
+        self._test_arr[2] = '1'
+        self.assertEqual(self.rpu.qbits.to01(), ''.join(self._test_arr))
+
+    def test_not_disjoints(self):
+        self.rpu.qbits.invert(3)
+        self.rpu.qbits.invert(4)
+        part = functools.partial(self.rpu.apply, RGate.NOT, {3, 4}, {4, 5})
+        self.assertRaises(ValueError, part)
+
+    def test_mcmnot(self):
+        trgts = {1, 3, 4, 9}
+        ctrls = {2, 5}
+        for i in ctrls:
+            self.rpu.qbits.invert(i)
+            self._test_arr[i] = '1'
+        for i in trgts:
+            self._test_arr[i] = '1'
+        self.rpu.apply(RGate.NOT, ctrls, trgts)
+        self.assertEqual(self.rpu.qbits.to01(), ''.join(self._test_arr))
+
+    def test_program_to_rprogram_error(self):
+        pr = Program()
+        pr.apply(H, pr.qalloc(1))
+
+        part = functools.partial(RProgram.program_to_rprogram, pr)
+        self.assertRaises(AttributeError, part)
+
+    def test_program_to_rprogram(self):
+        pr = Program()
+        qr = pr.qalloc(5)
+        pr.apply(X, qr[0])
+        pr.apply(X, qr[4])
+        pr.apply(SWAP, qr[4], qr[3])
+        pr.apply(CNOT, qr[:2])
+        pr.apply(CCNOT, qr[:3])
+        pr.apply(SWAP, qr[2], qr[4])
+        pr.apply(CCNOT, qr[2:5])
+        pr.apply(SWAP.ctrl(3), qr)
+        qpu = PyLinalg()
+        res = qpu.submit(pr.to_circ().to_job())
+        sample = None
+        for sample in res:
+            pass
+        self.assertNotEqual(sample, None)
+        rpr = RProgram.program_to_rprogram(pr)
+        print()
+        self.assertEqual(sample.state.bitstring, rpr.qbits.to01())
