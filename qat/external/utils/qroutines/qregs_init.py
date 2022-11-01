a748bb802a8252f1a1269343ad166d46e9b60bee
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-11-01 16:59:16 +0100

    FIX Multilple build gates on qbit_init

--- qat/external/utils/qroutines/qregs_init.py
+++ qat/external/utils/qroutines/qregs_init.py
@@ -81,7 +81,7 @@ def conditionally_initialize_qureg_to_complement_of_bitarray(
 
 
 # @build_gate("QBIT_INIT_BITA", [Union[List, nptyping.NDArray], bool])
-@build_gate("QBIT_INIT_BITA", [List, bool])
+# @build_gate("QBIT_INIT_BITA", [List, bool])
 def initialize_qureg_given_bitarray(a_str, little_endian) -> QRoutine:
     """Given a binary string, initialize the qreg to the proper value
     corresponding to it. Basically, if a_str is 1011, the function negate bits
