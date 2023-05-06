fb03d2160305284a3d50be520f0cca32002cef02
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2023-05-06 17:31:00 +0200

    ADD - indexing test

--- test/test_qroutine_bix.py
+++ test/test_qroutine_bix.py
@@ -32,66 +32,76 @@ class BixTestCase(CircuitTestCase):
         # the additional + 1 is required since we want indexes starting from 1
         # to n (and not the traditional 0 to n-1)
         # add should be 0 for not idx_start_at_one
-        add = 1
-        l2n = int(ceil(log2(n + add)))
-        onesexp = [
-            bin(i + add)[2:].zfill(l2n) for i, j in enumerate(bitstring) if j == "1"
-        ]
-        zerosexp = [
-            bin(i + add)[2:].zfill(l2n) for i, j in enumerate(bitstring) if j == "0"
-        ]
-        weight = len(onesexp)
-        reg_names = {}
+        for index_start_at_one in (True, False):
+            with self.subTest(index_start_at_one=index_start_at_one):
+                add = 1 if index_start_at_one else 0
+                l2n = int(ceil(log2(n + add)))
+                onesexp = [
+                    bin(i + add)[2:].zfill(l2n)
+                    for i, j in enumerate(bitstring)
+                    if j == "1"
+                ]
+                zerosexp = [
+                    bin(i + add)[2:].zfill(l2n)
+                    for i, j in enumerate(bitstring)
+                    if j == "0"
+                ]
+                weight = len(onesexp)
+                reg_names = {}
 
-        pr = Program()
-        wreg = pr.qalloc(n)
-        reg_names[f"wreg"] = range(wreg.start, wreg.length)
-        oregs = []
-        zregs = []
-        for i in range(weight):
-            qr = pr.qalloc(l2n)
-            oregs.append(qr)
-            reg_names[f"oregs_{i}"] = range(qr.start, qr.start + qr.length)
-        for i in range(n - weight):
-            qr = pr.qalloc(l2n)
-            zregs.append(qr)
-            reg_names[f"zregs_{i}"] = range(qr.start, qr.start + qr.length)
+                pr = Program()
+                wreg = pr.qalloc(n)
+                reg_names[f"wreg"] = range(wreg.start, wreg.length)
+                oregs = []
+                zregs = []
+                for i in range(weight):
+                    qr = pr.qalloc(l2n)
+                    oregs.append(qr)
+                    reg_names[f"oregs_{i}"] = range(qr.start, qr.start + qr.length)
+                for i in range(n - weight):
+                    qr = pr.qalloc(l2n)
+                    zregs.append(qr)
+                    reg_names[f"zregs_{i}"] = range(qr.start, qr.start + qr.length)
 
-        pr.apply(
-            qregs_init.initialize_qureg_given_bitstring(bitstring, little_endian=False),
-            wreg,
-        )
+                pr.apply(
+                    qregs_init.initialize_qureg_given_bitstring(
+                        bitstring, little_endian=False
+                    ),
+                    wreg,
+                )
 
-        qfun = bix.bix_fixed_weight(n, weight, True)
-        pr.apply(qfun, wreg, *oregs, *zregs)
+                qfun = bix.bix_fixed_weight(n, weight, index_start_at_one)
+                pr.apply(qfun, wreg, *oregs, *zregs)
 
-        circ = pr.to_circ(link=[cuccaro_arith.adder, cuccaro_arith.subtractor])
-        # circ = pr.to_circ(link=[tkk_arith.adder])
+                circ = pr.to_circ(link=[cuccaro_arith.adder, cuccaro_arith.subtractor])
+                # circ = pr.to_circ(link=[tkk_arith.adder])
 
-        obtained = None
-        if self.REVERSIBLE_ON:
-            rpr = RProgram.circuit_to_rprogram(circ, reg_names)
-            obtained = rpr.rbits.to01()
-        else:
-            res = self.qpu.submit(circ.to_job())
-            counts = len(res)
-            self.assertEqual(counts, 1)
-            for sample in res:
-                if self.SIMULATOR == "linalg":
-                    self.assertEqual(sample.probability, 1)
-                obtained = sample.state.bitstring
-        ones = []
-        zeros = []
-        for k, v in reg_names.items():
-            if k.startswith("oregs_"):
-                ones.append(obtained[v.start : v.stop])
-            elif k.startswith("zregs_"):
-                zeros.append(obtained[v.start : v.stop])
+                obtained = None
+                if self.REVERSIBLE_ON:
+                    rpr = RProgram.circuit_to_rprogram(circ, reg_names)
+                    obtained = rpr.rbits.to01()
+                else:
+                    res = self.qpu.submit(circ.to_job())
+                    counts = len(res)
+                    self.assertEqual(counts, 1)
+                    for sample in res:
+                        if self.SIMULATOR == "linalg":
+                            self.assertEqual(sample.probability, 1)
+                        obtained = sample.state.bitstring
+                ones = []
+                zeros = []
+                for k, v in reg_names.items():
+                    if k.startswith("oregs_"):
+                        ones.append(obtained[v.start : v.stop])
+                    elif k.startswith("zregs_"):
+                        zeros.append(obtained[v.start : v.stop])
 
-        self.assertEqual(obtained[wreg.start : wreg.length], bitstring)
-        for obt, exp in itertools.chain(zip(ones, onesexp), zip(zeros, zerosexp)):
-            self.assertEqual(obt, exp)
+                self.assertEqual(obtained[wreg.start : wreg.length], bitstring)
+                for obt, exp in itertools.chain(
+                    zip(ones, onesexp), zip(zeros, zerosexp)
+                ):
+                    self.assertEqual(obt, exp)
 
-        ancillae_start = zregs[-1].start + zregs[-1].length
-        for i in bitstring[ancillae_start:]:
-            self.assertEqual(i, "0")
+                ancillae_start = zregs[-1].start + zregs[-1].length
+                for i in bitstring[ancillae_start:]:
+                    self.assertEqual(i, "0")
