e33db497ea577ecf70a01c76d45c124e2030796b
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2024-04-02 19:11:22 +0200

    ADD Oracle measures

--- examples/kwan_des_sboxes.py
+++ examples/kwan_des_sboxes.py
@@ -306,6 +306,7 @@ def get_encryption_measures_sequential(n_keys):
     # * 16 bcz of no. of rounds of each U_E.
     # * uf_no bcz this has to be done for every U_E unitary.
     overall["G"] = {k: v * 2 * 16 * uf_no for k, v in compute["G"].items() if v > 0}
+    overall["G-sum"] = sum(overall["G"].values())
     # The CNOT required by EP, Key mix, and the L-S xor F(R)
     overall["G"]["CNOT"] = overall["G"]["CNOT"] + (16 + 48 + 32) * 2 * 16 * uf_no
     overall["D"] = compute["D"] * 2 * 16 * uf_no
@@ -314,6 +315,8 @@ def get_encryption_measures_sequential(n_keys):
     overall2 = {}
     overall2["W"] = np.log2(overall["W"])
     overall2["G"] = {k: np.log2(v) for k, v in overall["G"].items()}
+    # input(overall['G-sum'])
+    overall2["G-sum"] = np.log2(overall["G-sum"])
     overall2["D"] = np.log2(overall["D"])
     overall2["D-CCNOT"] = np.log2(overall["D-CCNOT"])
 
@@ -345,81 +348,104 @@ def get_encryption_measures_sequential(n_keys):
     return overall, overall2, cliffordt, cliffordt2
 
 
-def get_oracle_measures(n_keys):
+def get_oracle_parallel_measures(n_keys):
     """Overall measures of the Grover oracle, retrieved starting from the
     single compute measures of the previous function, and the number of grover
     iterations
 
     """
-    compute = get_sboxes_parallel_measures()
+    overall, overall2, cliffordt, cliffordt2 = get_encryption_measures_parallel(n_keys)
+    # 55 instead of 56 bcz of complimentary property of DES and 3DES; the
+    # logarithmic factor is due to the optimal no. of iterations
+    iters = (n_keys * 55 / 2) + np.log2(0.58)
 
-    # number of times we have to execute the U_f unitary. It's 1 for DES and 3
-    # for 3DES(-2 or -3)
-    uf_no = 1 if n_keys == 1 else 3
-
-    overall = {}
-    # Not multiplied by uf_no since the U_F and U_F^\dagger of each U_E will
-    # leave, in the end, the ancillary qubits clean, and they can be reused. We
-    # just have to add to the qubits count the keys and the left part of the
-    # plaintext.
-    width = compute["W"] + n_keys * 56 + 32
-    # * 2 bcz U_F and U_F^\dagger of each round.
-    # * 16 bcz of no. of rounds of each U_E.
-    # * 2 bcz of compute/uncompute (that is, U_f and U_f^\dagger) of oracle.
-    # * uf_no bcz this has to be done for every U_f unitary.
-    overall["G"] = {k: v * 2 * 16 * 2 * uf_no for k, v in compute["G"].items() if v > 0}
     # init + 2 to implement the CZ of the oracle + diffusion
     overall["G"]["H"] = n_keys + 2 + n_keys * 2
-    # The X required at each diffusion
-    overall["G"]["X"] = overall["G"]["X"] + n_keys * 2
-
-    # The CNOT required by EP, Key mix, and the L-S xor F(R)
-    overall["G"]["CNOT"] = overall["G"]["CNOT"] + (16 + 48 + 32) * 2 * 16 * 2 * uf_no
-    # the log factors are for the reflection decompositions
-    overall["D"] = (
-        compute["D"] * 2 * 16 * 2 * uf_no + 2 * np.log2(64) + 2 * np.log2(n_keys * 56)
-    )
+    # The X required to XOR the given ctx, estimated to be half of the ctx length
+    overall["G"]["X"] = overall["G"]["X"] + 32 * 2
     # The decomposition of the CCX for the 2 reflections will use:
     # for DES: 1) oracle, 62 bits; 2) diffusion: 56 qubits
     # for 3DES: 1) oracle, 62 bits; 2) diffusion: 56*2 or 56*3 qubits
     #
     # In all cases, we can reuse the ancillary qubits of the S-boxes, and we
     # don't need any additional one.
-    width += 0
-    overall["W"] = width
+    n_ccnot_phaseflip_decomposition = (64 - 1) * 2
+    overall["G"]["C-C-X"] = overall["G"]["C-C-X"] + n_ccnot_phaseflip_decomposition
+    overall["G-sum"] = sum(overall["G"].values())
+
+    # the log factors are for the reflection decompositions
+    overall["D"] = overall["D"] + np.log2(64)
+    overall["D-CCNOT"] = overall["D-CCNOT"] + np.log2(64)
 
     overall2 = {}
     overall2["W"] = np.log2(overall["W"])
-    overall2["G"] = {k: np.log2(v) for k, v in overall["G"].items()}
-    overall2["D"] = np.log2(overall["D"])
-    # 55 instead of 56 bcz of complimentary property of DES and 3DES
-    # logarithmic factor is the optimal no. of iterations
-    overall2["iters"] = (n_keys * 55 / 2) + np.log2(0.58)
+    overall2["G"] = {k: np.log2(v) + iters + 1 for k, v in overall["G"].items()}
+    overall2["G-sum"] = np.log2(overall["G-sum"]) + iters + 1
+    overall2["D"] = np.log2(overall["D"]) + iters + 1
+    overall2["D-CCNOT"] = np.log2(overall["D"]) + iters + 1
+
+    # Each CCX of the phase flip requires 10 Clifford for QAND, and 3 Clifford
+    # + 2 classically controlled Clifford for QAND^\dagger. We can just add,
+    # for the classically controlled, half of them (i.e., 1), since the
+    # classical qubit is on half of the time. We do not need any additional
+    # qubit for the oracle phase flip, since we can reuse the ones used for
+    # S-boxes (both ancillary and QAND related).
+
+    # non T gates, i.e., Clifford
+    cliffordt["nT"] += n_ccnot_phaseflip_decomposition / 2 * 14
+    # Divided by 2 since half of them is QAND and half is QAND^\dagger. The
+    # QAND requires 4 T gates.
+    cliffordt["T"] += n_ccnot_phaseflip_decomposition * 4 / 2
+    cliffordt["D-T"] += np.log2(64)
 
     cliffordt2 = {}
     # for each CCX of the S-boxes, we add 1 bit to have a T-depth 1
     # decomposition.
-    width = overall["W"] + compute["G"]["C-C-X"]
-    cliffordt2["W"] = np.log2(width)
+    cliffordt2["W"] = np.log2(cliffordt["W"])
     # Divided by 2 since half of them is QAND and half is QAND^\dagger
-    cliffordt2["T"] = overall2["G"]["C-C-X"] - 1
+    cliffordt2["nT"] = np.log2(cliffordt["nT"]) + iters + 1
+    cliffordt2["T"] = np.log2(cliffordt["T"]) + iters + 1
     # same as before, uncompute stage doesn't count
-    cliffordt2["D-T"] = np.log2(
-        compute["D-CCNOT"] * 2 * 16 * uf_no + 2 * np.log2(64) + 2 * np.log2(n_keys * 56)
-    )
-    cliffordt2["iters"] = overall2["iters"]
+    cliffordt2["D-T"] = np.log2(cliffordt["D-T"]) + iters + 1
 
-    return overall, overall2, cliffordt2
+    return overall, overall2, cliffordt, cliffordt2
+
+
+def get_oracle_sequential_measures(n_keys):
+    """Overall measures of the Grover oracle, retrieved starting from the
+    single compute measures of the previous function, and the number of grover
+    iterations.
+
+    """
+    # The decomposition of the CCX for the oracle reflection uses 62 bits. For
+    # the sequential case, we can reuse (some of) the ancillary qubits of the
+    # S5 box, and we don't need any additional one. For the decomposition of
+    # the CCX into QAND and QAND^\dagger, the encryption function added a no.
+    # of additional qubits equal to all the CCX contained in the S boxes to
+    # allow parallelization. However, we only want the maximum of all the S
+    # boxes here (62) for the CCNOT measures, while we multiply it by 2 for the
+    # QAND measures.
+
+    overall, overall2, cliffordt, cliffordt2 = get_oracle_parallel_measures(n_keys)
+    # n_ccnot_phaseflip_decomposition = (64 - 1) * 2
+
+    overall["W"] -= 59 + 52 + 53 + 38 + 58 + 53 + 53 + 50
+    overall["W"] += 62
+    overall2["W"] = np.log2(overall["W"])
+
+    cliffordt["W"] = overall["W"] + 62
+    cliffordt2["W"] = np.log2(cliffordt["W"])
+
+    return overall, overall2, cliffordt, cliffordt2
 
 
 def print_comparison_ciphers_measures():
     # Jaques
     row = "AES-128~\\cite{jaques2019implementing}"
+    row = "AES-128~\\cite{jaques2019implementing}"
     # Table 9 10
     row = "Camellia-128~\\cite{lin2023FurtherInsightsConstructing}"
-    row += (
-        "& 388 & 3566 & 39600 & 10816 & - & 5284 & 2050192 & 388 & 140510 & 68320 & 22188 & 6150576 \\\\ "
-    )
+    row += "& 388 & 3566 & 39600 & 10816 & - & 5284 & 2050192 & 388 & 140510 & 68320 & 22188 & 6150576 \\\\ "
     print(row)
     # Table 11
     row = "Camellia-128~\\cite{lin2023FurtherInsightsConstructing}"
@@ -428,6 +454,11 @@ def print_comparison_ciphers_measures():
     row = "SEED\\cite{oh2023OptimizedQuantumImplementation}"
     row += f"& 41496 & 8116 & 409520 & 41392 & 11837 & 321 & 13320216"
     row += f"& 41496 & 784740 & 289680 & 1284 & 53280864"
+    row += "\\\\"
+    print(row)
+
+    row = "HIGHT 64/128~\\cite{jang2022ParallelQuantumAddition}"
+    row += "& 228 & 4496 & 22614 & 5824 & 2479 & - & - & - & - & - & - & - "
     row += "\\\\"
     print(row)
 
@@ -492,30 +523,41 @@ def main():
     print("*" * 79)
     print("Full Grover")
     print("*" * 79)
-    header = f"Variant & W & X & CX & CCX & D & D$\\times$W & W & T & T-D & T-D$\\times$W\\\\"
+    # header = f"Variant & W & X & CX & CCX & D & {{Total D\\\\$\\cdot$W}} & W & T & T-D & T-D$\\cdot$W\\\\"
+    # header = f"Variant & W & {{Total\\\\Gates}} & {{Total\\\\Depth}} & {{Total Depth\\\\$\\cdot$Width}} & {{NIST\\\\Compl.}} & W & {{T}} & {{T-D}} & {{T-D\\\\$\\cdot$W}} & {{NIST\\\\Compl.}} \\\\"
+    header = f"Variant & W & G & D & D$\\cdot$W & {{NIST\\\\Compl.}} & W & T & T-D & T-D$\\cdot$W & {{NIST\\\\Compl.}} \\\\"
     print(header)
     for n_keys in range(1, 4):
-        _, overall2, cliffordt2 = get_oracle_measures(n_keys)
-        if n_keys == 1:
-            head = "DES"
-        elif n_keys == 2:
-            head = "3DES2"
-        elif n_keys == 3:
-            head = "3DES3"
-        iters = overall2["iters"]
-        row = f"{head} &"
-        row += f"{round(overall2['W'])} & "
-        row += f"{round(overall2['G']['X'] + iters)} & "
-        row += f"{round(overall2['G']['CNOT'] + iters)} & "
-        row += f"{round(overall2['G']['C-C-X'] + iters)} & "
-        row += f"{round(overall2['D'] + iters)} & "
-        row += f"{round(overall2['D'] + iters + overall2['W'])} & "
+        for suffix, res in zip(
+            ("\\TblrNote{$\\lozenge$}", "\\TblrNote{$\\blacklozenge$}"),
+            (
+                get_oracle_sequential_measures(n_keys),
+                get_oracle_parallel_measures(n_keys),
+            ),
+        ):
+            overall, overall2, cliffordt, cliffordt2 = res
+            if n_keys == 1:
+                head = f"DES{suffix}"
+            elif n_keys == 2:
+                head = f"3DES2{suffix}"
+            elif n_keys == 3:
+                head = f"3DES3{suffix}"
+            row = f"{head} "
+            row += f"& {round(overall2['W'])} "
+            # row += f"& {round(overall2['G']['X'] )}"
+            # row += f"& {round(overall2['G']['CNOT'] )}"
+            # row += f"& {round(overall2['G']['C-C-X'] )}"
+            row += f"& {round(overall2['G-sum'] )} "
+            row += f"& {round(overall2['D'] )} "
+            row += f"& {round(overall2['D']  + overall2['W'])} "
+            row += f"& {round(overall2['D'] + overall2['G-sum'])}"
 
-        row += f"{round(cliffordt2['W'])} & "
-        row += f"{round(cliffordt2['T'] + iters)} & "
-        row += f"{round(cliffordt2['D-T'] + iters)} & "
-        row += f"{round(cliffordt2['D-T'] + iters + overall2['W'])}\\\\"
-        print(row)
+            row += f"& {round(cliffordt2['W'])} "
+            row += f"& {round(cliffordt2['T'] )} "
+            row += f"& {round(cliffordt2['D-T'] )} "
+            row += f"& {round(cliffordt2['D-T']  + overall2['W'])}"
+            row += f"& {round(cliffordt2['D-T'] + overall2['G-sum'])}\\\\"
+            print(row)
 
     # ex_sall()
 
