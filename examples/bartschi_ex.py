740b3405c6a2b7c5c7de0d664d5232a22f1bb016
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-03-09 09:19:26 +0100

    MOD bartschi ex

--- examples/bartschi_ex.py
+++ examples/bartschi_ex.py
@@ -1,19 +1,15 @@
-from qatext.qroutines.hamming_weight_generate import bartschiE19
 from qat.core.console import display
-from qat.lang.AQASM import *
-
-from qat.qpus import PyLinAlg
+from qat.lang.AQASM.program import Program
+from qatext.qroutines.hamming_weight_generate import bartschiE19
 
 
 def main():
-    n=4
-    w=2
+    n = 4
+    w = 2
     pr = Program()
     qr = pr.qalloc(n)
     pr.apply(bartschiE19.generate(n, w), qr)
     display(pr.to_circ(), max_depth=3)
-
-
 
 
 if __name__ == '__main__':
