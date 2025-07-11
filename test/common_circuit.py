3a61f379f23914a3d3ab9b268dde13c50dad87a7
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-07-11 17:33:08 +0200

    ADD common circuit function to get/print rprogram regs

--- test/common_circuit.py
+++ test/common_circuit.py
@@ -3,6 +3,7 @@ from test.common import BasicTestCase
 from typing import TYPE_CHECKING
 
 from qat.core.console import display
+from qatext.utils.bits.conversion import get_ints_from_bitarray
 
 if TYPE_CHECKING:
     from qat.core.wrappers.circuit import Circuit
@@ -29,35 +30,35 @@ class CircuitTestCase(BasicTestCase):
         cls.links = []
         if cls.SIMULATOR.lower() == "pylinalg":
             cls.logger.info("PyLinalg")
-            from qat.pylinalg import PyLinalg
+            from qat.pylinalg import PyLinalg  # type:ignore
 
             cls.qpu = PyLinalg()
         elif cls.SIMULATOR.lower() == "linalg":
             # default to linalg
-            from qat.qpus import LinAlg
+            from qat.qpus import LinAlg  # type:ignore
 
             cls.logger.info("LinAlg")
             cls.qpu = LinAlg()
         elif cls.SIMULATOR.lower() == "stabs":
             cls.logger.info("Stabs")
+            from qat.qpus import Stabs  # type:ignore
             from qatext.synthesis.mctrls.mcx import ccnot, x
-            from qat.qpus import Stabs
 
             cls.qpu = Stabs()
             cls.links = [ccnot, x]
         elif cls.SIMULATOR.lower() == "feynman":
             cls.logger.info("Feynman")
-            from qat.qpus import Feynman
+            from qat.qpus import Feynman  # type:ignore
 
             cls.qpu = Feynman()
         elif cls.SIMULATOR.lower() == "mps":
             cls.logger.info("MPS")
-            from qat.qpus import MPS
+            from qat.qpus import MPS  # type:ignore
 
             cls.qpu = MPS(lnnize=True)
         elif cls.SIMULATOR.lower() == "bdd":
             cls.logger.info("BDD")
-            from qat.qpus import Bdd
+            from qat.qpus import Bdd  # type:ignore
 
             cls.qpu = Bdd(48)
         else:
@@ -96,3 +97,26 @@ class CircuitTestCase(BasicTestCase):
     def print_result(result: "Result"):
         for sample in result:
             print(sample)
+
+    @staticmethod
+    def get_rprogram_regs(pr: "Program", reg_name_to_slice, link: list):
+        from qatext.qpus.reversible import get_states_from_program
+        res = get_states_from_program(pr, reg_name_to_slice, link=link)
+        return res
+
+    @staticmethod
+    def print_rprogram_regs(pr: "Program", reg_name_to_slice, reg_name_to_size,
+                            link):
+        res = CircuitTestCase.get_rprogram_regs(pr, reg_name_to_slice, link)
+        for k, v in res.items():
+            print(k, end=": ")
+            if k == 'wreg':
+                print(v)
+            else:
+                n, m = reg_name_to_size[k]
+                if n == -1:
+                    val = v
+                else:
+                    val = get_ints_from_bitarray(v, n, m, False)
+                print(val)
+        return res
