4dbebe5fb2197603a6cbe0f5230f912dfbd53684
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-07-21 14:19:24 +0200

    Minor typings

--- qatext/qpus/common.py
+++ qatext/qpus/common.py
@@ -5,9 +5,9 @@ class QRegsProperties(NamedTuple):
     # This is for 1 or more collection of qregs
     slic: slice
     # number of qregs aggregated
-    n: int
+    n: int | None
     # size of each qreg
-    m: int
+    m: int | None
     qtype: Type[Union[int, str]]
     # if True, should set the slice stop to -1, and n to -1, and m to -1
     unknown_size: bool = False
