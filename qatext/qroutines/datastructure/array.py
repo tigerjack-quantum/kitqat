26403b754241dd8ce74bb764d1acadde494f5eeb
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-08-13 21:21:07 +0200

    MOD array

--- qatext/qroutines/datastructure/array.py
+++ qatext/qroutines/datastructure/array.py
@@ -2,7 +2,6 @@ import logging
 import operator
 from functools import reduce
 
-from qat.lang.AQASM.gates import CNOT
 from qat.lang.AQASM.misc import build_gate
 from qat.lang.AQASM.routines import QRoutine
 from qatext.qatmgmt.routines import QRoutineWrapper
@@ -13,8 +12,7 @@ from qatext.qatmgmt.routines import QRoutineWrapper
 LOGGER = logging.getLogger(__name__)
 
 
-@build_gate("ARRAY_CONTAINS", [int, int, bool],
-            lambda n, m, _: n * m + m + 1)
+@build_gate("ARRAY_CONTAINS", [int, int, bool], lambda n, m, _: n * m + m + 1)
 def contains(n, m, repeated_values_possible=False):
     """Given an array of quantum registers of $n$ cells, each one having $m$
     qubits, returns 1 on an output qubit if the array contains a specific value.
@@ -30,19 +28,19 @@ def contains(n, m, repeated_values_possible=False):
     qarray = routinew.qarray_wires(n, m, "array", int)
     qbit_out = routinew.qarray_wires(1, 1, "out", str)[0][0]
     # it will contain the 0/1 bit of the comparisons
-    qarray_cmp = routinew.qarray_wires(1, n, "array_cmp", bool)[0]
-    routinew.set_ancillae(qarray_cmp)
 
-    with routinew.compute():
-        for qreg_val, qbit_cmp in zip(qarray, qarray_cmp):
-            (qreg_val == qreg_value_to_check).evaluate(output=qbit_cmp)
-    if not repeated_values_possible:
-        for i in range(n):
-            routinew.apply(CNOT, qarray_cmp[i], qbit_out)
-    else:
-        or_formula = reduce(operator.or_, qarray_cmp)
+    if repeated_values_possible:
+        qreg_cmp = routinew.qarray_wires(1, n, "array_cmp", bool)[0]
+        routinew.set_ancillae(qreg_cmp)
+        with routinew.compute():
+            for qreg_val, qbit_cmp in zip(qarray, qreg_cmp):
+                (qreg_val == qreg_value_to_check).evaluate(output=qbit_cmp)
+        or_formula = reduce(operator.or_, qreg_cmp)
         or_formula.evaluate(output=qbit_out)
+        routinew.uncompute()
 
-    routinew.uncompute()
+    else:
+        for qreg_val in qarray:
+            (qreg_val == qreg_value_to_check).evaluate(output=qbit_out)
 
     return routinew
